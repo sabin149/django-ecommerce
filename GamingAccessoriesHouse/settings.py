60598f554dff43f7204877f0fab2c1ea7161c44c
sabin149 <58678988+sabin149@users.noreply.github.com>
2021-03-28 21:30:35 +0545

    admins app created

--- GamingAccessoriesHouse/settings.py
+++ GamingAccessoriesHouse/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app','bootstrapform'
+    'app','bootstrapform','admins'
 ]
 
 MIDDLEWARE = [
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Kathmandu'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
