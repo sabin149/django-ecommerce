24d78115824f57384d47f9fddc559b8801d47c04
sabin149 <58678988+sabin149@users.noreply.github.com>
2021-04-03 14:29:15 +0545

    homeview updated

--- app/views.py
+++ app/views.py
@@ -11,34 +11,38 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import JsonResponse
  
-
 class HomeView(TemplateView):
-    template_name = "app/home.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get(self,request):
+        totalitem = 0
+        if request.user.is_authenticated:
+            totalitem = len(Cart.objects.filter(user=request.user))
         all_products = Product.objects.all().order_by("-id")
         paginator = Paginator(all_products, 8)
         page_number = self.request.GET.get('page')
         product_list = paginator.get_page(page_number)
-        context['product_list'] = product_list
-        return context
+        template_name = "app/home.html"
+        context={'product_list':product_list,'totalitem':totalitem}
+        return render(request,template_name,context)
+
 
 class AllProductsView(TemplateView):
-    template_name = "app/allproducts.html"
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['allcategories'] = Category_choices.objects.all()
-        return context
+    def get(self,request):
+        totalitem = 0
+        if self.request.user.is_authenticated:
+            totalitem = len(Cart.objects.filter(user=request.user))
+        template_name = "app/allproducts.html"
+        allcategories=Category_choices.objects.all()
+        context={'allcategories':allcategories,'totalitem':totalitem}
+        return render(request,template_name,context)
 
 
 class ProductDetailView(TemplateView):
-    def get(self, request, *args, **kwargs):
-        url_slug = self.kwargs['slug']
-        totalitem = 0
+    def get(self, request, **kwargs):
+        url_slug = kwargs['slug'] 
         product = Product.objects.get(slug=url_slug)
         item_already_in_cart = False
-        if request.user.is_authenticated:
+        totalitem = 0
+        if self.request.user.is_authenticated:
             totalitem = len(Cart.objects.filter(user=request.user))
             item_already_in_cart = Cart.objects.filter(
                 Q(product=product.id) & Q(user=request.user)).exists()
@@ -46,15 +50,16 @@ class ProductDetailView(TemplateView):
     
 
 class SearchView(TemplateView):
-    template_name = "app/search.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get(self,request):
+        totalitem = 0
+        if request.user.is_authenticated:
+            totalitem = len(Cart.objects.filter(user=request.user))
         kw = self.request.GET.get("keyword")
         results = Product.objects.filter(
             Q(title__icontains=kw) | Q(description__icontains=kw) | Q(brand__icontains=kw))
-        context["results"] = results
-        return context
+        template_name = "app/search.html"
+        context={'results':results,'totalitem':totalitem}
+        return render(request,template_name,context)
 
 
 @unauthenticated_user
